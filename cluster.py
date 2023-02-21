66094ed3efe18da387792d9aa04c79db7195a5d9
Tiago Oliveira <ti.olive@gmail.com>
2023-02-21 02:20:45 +0100

    fixed basic structure

--- cluster.py
+++ cluster.py
@@ -5,10 +5,17 @@ from collections import Counter
 #from redisearch import Client
 import redis
 import json
-
 import ray
-
 import os
+
+ray.init()
+
+f = open(
+    '/Users/tiagoooliveira/Documents/dev/scala/akka-http-quickstart-scala/src/main/resources/patent-13062022-1.json')
+data = json.load(f)
+f.close()
+
+database_object_ref = ray.put(data)
 
 @ray.remote
 def get_host_name(x):
@@ -24,8 +31,9 @@ def send_to_redis(record):
     #https://pypi.org/project/redisearch/
 
 @ray.remote
-def retrieve_task(item, database_object_ref=None):
-    obj_store_data = ray.get(database_object_ref) time.sleep(item / 10.)
+def retrieve_task(item):
+    obj_store_data = ray.get(database_object_ref)
+    time.sleep(item / 10.)
     return item, obj_store_data[item]
 
 def wait_for_nodes(expected):
@@ -48,9 +56,11 @@ def print_runtime(input_data, start_time, decimals=1):
     print(*input_data, sep='\n')
 
 def main():
+
     #https://realpython.com/python-redis/
     #wait_for_nodes(4)
 
+    #curl -O --insecure -I https://files.pythonhosted.org/packages/ba/7d/2ae672b176e675519c0f5d2cb46f023e37be3754a59f7307756e3fdf7552/redisearch-2.1.1-py3-none-any.whl
     #r = redis.Redis(host=os.environ["REDIS_ENDPOINT"])
 
     #loading from file
@@ -58,24 +68,18 @@ def main():
     #iterate over remote method/actor (responsible to send json to Redis) at the limit of actors available
 
 
-    f = open('/Users/tiagoooliveira/Documents/dev/scala/akka-http-quickstart-scala/src/main/resources/patent-13062022-1.json')
-    data = json.load(f)
-    f.close()
-
-    database_object_ref = ray.put(data)
-
     start = time.time()
     #data_references = [retrieve_task.remote(item) for item in range(8)]
-    data_references = retrieve_task.remote("CN112310387B", database_object_ref)
+    data_references = retrieve_task.remote("CN112310387B")
     all_data = []
 
-    while len(data_references) > 0:
-        finished, data_references = ray.wait(data_references)
-        data = ray.get(finished)
+
+    finished, data_references = ray.wait(data_references)
+    data = ray.get(finished)
 
     print_runtime(data, start, 3)
 
-    all_data.extend(data)
+    #all_data.extend(data)
 
     print("Success!")
     sys.stdout.flush()
@@ -83,6 +87,4 @@ def main():
 
 
 if __name__ == "__main__":
-    #ray.init(address="localhost:6379")
-    ray.init()
     main()
